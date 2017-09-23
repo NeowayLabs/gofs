e796226218ac8a3e11fedb61aabbc7eb4a5688ef
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-09-23 19:01:44 -0300

    Add basic write validation

--- fs_test.go
+++ fs_test.go
@@ -1,7 +1,11 @@
 package gofs_test
 
 import (
+	"fmt"
+	"io"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/NeowayLabs/gofs"
 )
@@ -27,11 +31,11 @@ func testFS(t *testing.T, newfs fsBuilder) {
 	})
 
 	t.Run("Remove", func(t *testing.T) {
-		testRemove(t, newfs)
+		testRemoveFile(t, newfs)
 	})
 
 	t.Run("RemoveNonExistent", func(t *testing.T) {
-		testRemoveNonExistent(t, newfs)
+		testRemoveFileNonExistent(t, newfs)
 	})
 
 	t.Run("ReadNonExistent", func(t *testing.T) {
@@ -40,6 +44,20 @@ func testFS(t *testing.T, newfs fsBuilder) {
 }
 
 func testReadWrite(t *testing.T, newfs fsBuilder) {
+	fs := newfs(t)
+	path := newtestpath()
+	writer, err := fs.Create(path)
+
+	assertNoError(t, err, "error creating file[%s]", path)
+	defer closeIO(t, writer)
+
+	expectedContents := []byte(path)
+	n, err := writer.Write(expectedContents)
+	assertNoError(t, err, "error writing file[%s]", path)
+
+	if n != len(expectedContents) {
+		t.Fatal("expected to write[%d], wrote just[%d]", n, len(expectedContents))
+	}
 }
 
 func testReadWriteAll(t *testing.T, newfs fsBuilder) {
@@ -51,11 +69,38 @@ func testTruncatingExistentPath(t *testing.T, newfs fsBuilder) {
 func testCloseTwice(t *testing.T, newfs fsBuilder) {
 }
 
-func testRemove(t *testing.T, newfs fsBuilder) {
+func testRemoveFile(t *testing.T, newfs fsBuilder) {
 }
 
-func testRemoveNonExistent(t *testing.T, newfs fsBuilder) {
+func testRemoveFileNonExistent(t *testing.T, newfs fsBuilder) {
 }
 
 func testReadNonExistent(t *testing.T, newfs fsBuilder) {
+}
+
+func assertNoError(t *testing.T, err error, args ...interface{}) {
+	t.Helper()
+
+	if err != nil {
+		errmsg := ""
+		if len(args) > 0 {
+			errmsg = fmt.Sprintf(args[0].(string), args[1:]...)
+		}
+		t.Fatalf("unexpected error[%s] %s", err, errmsg)
+	}
+}
+
+func newtestpath() string {
+	return fmt.Sprintf(
+		"gofs-%d-%d",
+		time.Now().Unix(),
+		rand.Intn(99999999),
+	)
+}
+
+func closeIO(t *testing.T, closer io.Closer) {
+	err := closer.Close()
+	if err != nil {
+		t.Fatal(err)
+	}
 }
