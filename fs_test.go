fa299ec26d6145e6d71d5fd0d065841c648851af
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-09-23 17:54:46 -0300

    Fix codecov integration

--- fs_test.go
+++ fs_test.go
@@ -9,6 +9,34 @@ import (
 type fsBuilder func(t *testing.T) gofs.FS
 
 func testFS(t *testing.T, newfs fsBuilder) {
+
+	t.Run("ReadWrite", func(t *testing.T) {
+		testReadWrite(t, newfs)
+	})
+
+	t.Run("ReadWriteAll", func(t *testing.T) {
+		testReadWriteAll(t, newfs)
+	})
+
+	t.Run("TruncateExistentPath", func(t *testing.T) {
+		testTruncatingExistentPath(t, newfs)
+	})
+
+	t.Run("CloseTwice", func(t *testing.T) {
+		testCloseTwice(t, newfs)
+	})
+
+	t.Run("Remove", func(t *testing.T) {
+		testRemove(t, newfs)
+	})
+
+	t.Run("RemoveNonExistent", func(t *testing.T) {
+		testRemoveNonExistent(t, newfs)
+	})
+
+	t.Run("ReadNonExistent", func(t *testing.T) {
+		testReadNonExistent(t, newfs)
+	})
 }
 
 func testReadWrite(t *testing.T, newfs fsBuilder) {
@@ -27,4 +55,7 @@ func testRemove(t *testing.T, newfs fsBuilder) {
 }
 
 func testRemoveNonExistent(t *testing.T, newfs fsBuilder) {
+}
+
+func testReadNonExistent(t *testing.T, newfs fsBuilder) {
 }
