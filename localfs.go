57be3f0a7b59361bec2964bba76a65558347f31c
Tiago Katcipis <tiagokatcipis@gmail.com>
2017-10-08 21:03:53 -0300

    Add local file implementation

--- localfs.go
+++ localfs.go
@@ -1,30 +1,70 @@
 package gofs
 
-import "io"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
 
 type LocalFS struct {
+	basedir string
 }
 
 func (l *LocalFS) Open(path string) (io.ReadCloser, error) {
-	return nil, nil
+	fullpath := filepath.Join(l.basedir, path)
+	return os.Open(fullpath)
 }
 
 func (l *LocalFS) ReadAll(path string) ([]byte, error) {
-	return nil, nil
+	reader, err := l.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer reader.Close()
+	return ioutil.ReadAll(reader)
 }
 
 func (l *LocalFS) Create(path string) (io.WriteCloser, error) {
-	return nil, nil
+	fullpath := filepath.Join(l.basedir, path)
+	if err := createDirs(fullpath); err != nil {
+		return nil, fmt.Errorf("unable to create file[%s], error[%s]", path, err)
+	}
+	return os.Create(fullpath)
 }
 
 func (l *LocalFS) WriteAll(path string, contents []byte) error {
-	return nil
+	writer, err := l.Create(path)
+	if err != nil {
+		return err
+	}
+	defer writer.Close()
+	n, err := io.Copy(writer, bytes.NewReader(contents))
+	if n != int64(len(contents)) {
+		return fmt.Errorf("unable to write all, expected to write [%i] bytes but wrote[%i]", len(contents), n)
+	}
+	return err
 }
 
 func (l *LocalFS) Remove(path string) error {
-	return nil
+	fullpath := filepath.Join(l.basedir, path)
+	if _, err := os.Stat(fullpath); os.IsNotExist(err) {
+		return fmt.Errorf("error[%s] removing file[%s]", err, path)
+	}
+	return os.RemoveAll(fullpath)
 }
 
-func NewLocalFS() *LocalFS {
-	return &LocalFS{}
+// NewLocalFS creates a new LocalFS where all
+// files are opened and created relative to the
+// basedir provided. Very much like a chroot on
+// the provided basedir.
+func NewLocalFS(basedir string) *LocalFS {
+	return &LocalFS{basedir: basedir}
+}
+
+func createDirs(path string) error {
+	dir := filepath.Dir(path)
+	return os.MkdirAll(dir, 0766)
 }
--- localfs_test.go
+++ localfs_test.go
@@ -1,9 +1,16 @@
 package gofs_test
 
-import "testing"
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/NeowayLabs/gofs"
+)
 
 func TestLocalFS(t *testing.T) {
-	//testFS(t, func(t *testing.T) gofs.FS {
-	//return gofs.NewLocalFS()
-	//})
+	testFS(t, func(t *testing.T) gofs.FS {
+		dir, err := ioutil.TempDir("", "gofs")
+		assertNoError(t, err)
+		return gofs.NewLocalFS(dir)
+	})
 }
